71a14cd20fc04e011ba05ad00d05884aed476a51
Farras Faddila <35418377+donbasta@users.noreply.github.com>
2021-09-07 16:19:39 +0700

    Form (#1)
    * add axios
    
    * add util decode base64
    
    * Add constants for google stuff
    
    * Update API appscript
    
    * Upload file to drive
    
    * Handle null file

---
+++ src/constants/gform.js
@@ -0,0 +1,17 @@
+export const formEntry = {
+    name: "entry.1431541705",
+    NIM: "entry.796129122",
+    email: "entry.654249942",
+    idLine: "entry.77988022",
+    personalGoal: "entry.1975283871",
+    stuffToLearn: "entry.784396542",
+    pref1: "entry.698577386",
+    pref2: "entry.602033630",
+    commit: "entry.389783269",
+    cvUrl: "entry.1807738968"
+}
+
+const formID = "1gFr4tTkGWirrkZqdp8-P_kASwJpTMfeqsiVHKPIx8SI"
+export const formUrl = `https://docs.google.com/forms/d/${formID}/formResponse`
+
+export const apiUrl = `https://script.google.com/macros/s/AKfycbyMyG9GrOen9pAjSu0RA3wcT7dJgh-YPSJxhSIcN8mVHlfM2Ih7ffuT30UsaZX6w0_u/exec`;
--- src/pages/mentoring.js
+++ src/pages/mentoring.js
@@ -9,10 +9,17 @@ import {
   Snackbar,
 } from "@material-ui/core"
 import MuiAlert from "@material-ui/lab/Alert"
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Biodata from "../components/mentoring/biodata"
+import {
+  formEntry,
+  formUrl,
+  apiUrl
+} from "../constants/gform"
+import axios from "axios"
+import { encodeBase64 } from "../utils"
 import MentoringHead from "../components/mentoring/MentoringHead"
 
 const careerPaths = [
@@ -32,6 +39,8 @@ const careerPaths = [
 
 const Mentoring = () => {
   const [open, setOpen] = useState(false)
+  const [file, setFile] = useState(null)
+  const fileInput = useRef(null)
 
   const handleClick = () => {
     setOpen(true)
@@ -43,6 +52,56 @@ const Mentoring = () => {
     }
 
     setOpen(false)
+  }
+
+  const handleSubmit = (event) => {
+    const uploadedFile = (file) ? file[0] : null;
+
+    encodeBase64(uploadedFile)
+      .then(bin => {
+        if (bin === null) {
+          return "no CV uploaded"
+        }
+
+        bin = bin.split(',')[1];
+        const data = (bin);
+        
+        return axios.post(`${apiUrl}?filename=${uploadedFile.name}`, data)
+          .then(res => {
+            if (res.data.status === "SUCCESS") {
+              return res.data.fileUrl;
+            } else {
+              return null;
+            }
+          });
+      })
+      .then(res => {
+        console.log(res + " haha")
+        const url = formUrl
+    
+        let formData = new FormData(event.target)
+        formData.append(formEntry.cvUrl, res)
+    
+        const config = {
+          mode: "no-cors",
+          headers: {
+            'content-type': 'multipart/form-data'
+          }
+        }
+      
+        axios.post(url, formData, config)
+          .then(res => {
+            console.log(res)
+            handleClick()
+          })
+          .catch(err => {
+            console.log(err)
+            handleClick()
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   const isError = false
@@ -61,21 +120,26 @@ const Mentoring = () => {
             <form
               onSubmit={e => {
                 e.preventDefault()
-                handleClick()
+                handleSubmit(e)
               }}
             >
+              <div className="d-md-flex justify-content-between">
+                <TextField label="Name" className="name-input mt-4" name={formEntry.name}/>
+              </div>
               <div className="d-md-flex justify-content-between name-nim-id-container">
-                <TextField label="Name" className="name-input mt-4" />
+                <TextField label="Email" className="email-input mt-4" name={formEntry.email}/>
                 <div className="d-flex justify-content-between nim-id-container mt-4">
                   <TextField
                     label="NIM"
                     style={{ width: "47.5%" }}
                     className="nim-id-input"
+                    name={formEntry.NIM}
                   />
                   <TextField
                     label="ID Line"
                     style={{ width: "47.5%" }}
                     className="nim-id-input"
+                    name={formEntry.idLine}
                   />
                 </div>
               </div>
@@ -89,6 +153,7 @@ const Mentoring = () => {
                   style={{
                     width: "100%",
                   }}
+                  name={formEntry.personalGoal}
                 />
                 <Typography variant="caption">
                   Elaborasikan goal dua bulan yang ingin anda dicapai dengan
@@ -108,10 +173,11 @@ const Mentoring = () => {
                   style={{
                     width: "100%",
                   }}
+                  name={formEntry.stuffToLearn}
                 />
               </div>
 
-              <div className="d-md-flex justify-content-between mt-5">
+              <div className="d-flex justify-content-between mt-5">
                 <div className="pref-container mt-4">
                   <TextField
                     id="filled-select-currency"
@@ -121,6 +187,7 @@ const Mentoring = () => {
                     style={{
                       width: "100%",
                     }}
+                    name={formEntry.pref1}
                   >
                     {careerPaths.map(option => (
                       <MenuItem key={option.value} value={option.value}>
@@ -129,7 +196,6 @@ const Mentoring = () => {
                     ))}
                   </TextField>
                 </div>
-
                 <div className="pref-container mt-4">
                   <TextField
                     id="filled-select-currency"
@@ -137,8 +203,9 @@ const Mentoring = () => {
                     label="Second preference"
                     helperText="Please select your career path preference"
                     style={{
-                      width: "100%",
+                      width: "100.5%",
                     }}
+                    name={formEntry.pref2}
                   >
                     {careerPaths.map(option => (
                       <MenuItem key={option.value} value={option.value}>
@@ -151,16 +218,24 @@ const Mentoring = () => {
 
               <div className="mt-3">
                 <Typography variant="body1">CV (bila ada)</Typography>
-                <Button variant="contained" color="secondary" className="mt-2">
+                <input 
+                  type="file" 
+                  ref={fileInput}
+                  onChange={(e) => setFile(e.target.files)}
+                  style={{ display: "none" }}
+                />
+                <Button variant="contained" color="secondary" className="mt-2"
+                  onClick={() => fileInput.current.click()}
+                >
                   Upload File
-                  <input type="file" hidden />
                 </Button>
+                {file ? <Typography variant="body1">{file[0].name}</Typography> : null}
               </div>
 
               <div className="mt-5">
                 <Typography variant="body1">
-                  Apakah kamu bisa berkomitmen untuk menjalankan proses
-                  mentoring selama 2 bulan jika diterima ?
+                  Apakah kamu bisa berkomitmen untuk menjalankan proses mentoring
+                  selama 2 bulan jika diterima ?
                 </Typography>
                 <Typography variant="caption">
                   *TCD akan memegang uang deposit Rp100.000. Uang deposit tidak
@@ -170,29 +245,26 @@ const Mentoring = () => {
                 <div>
                   <FormControlLabel
                     control={
-                      <Checkbox
-                        name="checkedA"
-                        color="primary"
-                        required={true}
-                      />
+                      <Checkbox name={formEntry.commit} color="primary" required={true} value="Ya"/>
                     }
                     label="Jelas dong"
                     aria-required={true}
                   />
                 </div>
-              </div>
-              <div className="d-flex justify-content-center">
-                <Button
-                  color="primary"
-                  variant="contained"
-                  className="mt-5"
-                  type="submit"
-                  style={{
-                    textDecoration: "none",
-                  }}
-                >
-                  Submit Registration
-                </Button>
+
+                <div className="d-flex justify-content-center">
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    className="mt-5"
+                    type="submit"
+                    style={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    Submit Registration
+                  </Button>
+                </div>
               </div>
             </form>
           </FormGroup>
@@ -218,6 +290,10 @@ const Mentoring = () => {
       <style>
         {`
           .name-input {
+            width: 100%;
+          }
+
+          .email-input {
             width: 45%;
           }
 
