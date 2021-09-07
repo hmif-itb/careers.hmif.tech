5caaa9273d5659e5a4ff7903eded86198b37b2a0
donbasta <faddilafarras@gmail.com>
2021-09-08 00:07:02 +0700

    add several validations

--- src/components/mentoring/forms.js
+++ src/components/mentoring/forms.js
@@ -13,6 +13,7 @@ import axios from "axios"
 import { useAlert } from "../../contexts/AlertContext"
 import { formEntry, formUrl, apiUrl } from "../../constants/mentors"
 import { encodeBase64 } from "../../helpers/utils"
+import { REGEXP_NIM, MAX_FILE_SIZE_KB } from "../../constants/utils"
 
 const careerPaths = [
   {
@@ -32,6 +33,8 @@ const careerPaths = [
 const MentoringForm = () => {
   const [file, setFile] = useState(null)
   const fileInput = useRef(null)
+  const [pref1, setPref1] = useState(null)
+  const [pref2, setPref2] = useState(null)
 
   const { addAlert } = useAlert()
 
@@ -92,6 +95,14 @@ const MentoringForm = () => {
     }
   }
 
+  const handleFileUpload = (e) => {
+    if ((e.target.files.length > 0) && (e.target.files[0].size > MAX_FILE_SIZE_KB)) {
+      addAlert("File is too big, the maximum size is 2 MB")
+    } else {
+      setFile(e.target.files)
+    }
+  }
+
   return (
     <>
       <div style={{ maxWidth: "var(--maxWidth-4xl)", margin: "auto" }}>
@@ -121,6 +132,19 @@ const MentoringForm = () => {
                 required
                 className="nim-id-input"
                 name={formEntry.NIM}
+                inputProps={{ 
+                  pattern: REGEXP_NIM,
+                  onInvalid: e => {
+                    e.target.setCustomValidity("");
+                    if (!e.target.validity.valid) {
+                      e.target.setCustomValidity("Pastikan kamu anak HMIF angkatan 19 atau 20 ya, dan NIM-nya sudah benar!")
+                    }
+                  },
+                  onInput: e => {
+                    e.target.setCustomValidity("Pastikan kamu anak HMIF angkatan 19 atau 20 ya, dan NIM-nya sudah benar!")
+                    e.target.reportValidity();
+                  }
+                }}
               />
             </div>
             <div className="d-flex justify-content-between mt-4 justify-5-1 last">
@@ -130,6 +154,19 @@ const MentoringForm = () => {
                 name={formEntry.email}
                 required
                 variant="filled"
+                inputProps={{
+                  pattern: "^.+@.+$",
+                  onInvalid: e => {
+                    e.target.setCustomValidity("");
+                    if (!e.target.validity.valid) {
+                      e.target.setCustomValidity("Yang bener ya format emailnya")
+                    }
+                  },
+                  onInput: e => {
+                    e.target.setCustomValidity("Yang bener ya format emailnya")
+                    e.target.reportValidity();
+                  }
+                }}
               />
               <TextField
                 label="ID Line"
@@ -208,9 +245,10 @@ const MentoringForm = () => {
                   label="Preferensi pertama"
                   helperText="Pilih preferensi jalur mentoring pertama"
                   name={formEntry.pref1}
+                  onChange={e => setPref1(e.target.value)}
                 >
                   {careerPaths.map(option => (
-                    <MenuItem key={option.value} value={option.value}>
+                    <MenuItem key={option.value} value={option.value} disabled={option.value === pref2}>
                       {option.label}
                     </MenuItem>
                   ))}
@@ -224,9 +262,10 @@ const MentoringForm = () => {
                   label="Preferensi kedua"
                   helperText="Pilih preferensi jalur mentoring kedua"
                   name={formEntry.pref2}
+                  onChange={e => setPref2(e.target.value)}
                 >
                   {careerPaths.map(option => (
-                    <MenuItem key={option.value} value={option.value}>
+                    <MenuItem key={option.value} value={option.value} disabled={option.value === pref1}>
                       {option.label}
                     </MenuItem>
                   ))}
@@ -239,7 +278,7 @@ const MentoringForm = () => {
               <input
                 type="file"
                 ref={fileInput}
-                onChange={e => setFile(e.target.files)}
+                onChange={(e) => handleFileUpload(e)}
                 style={{ display: "none" }}
                 accept="application/pdf"
               />
