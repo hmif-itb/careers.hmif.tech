25ef0be283edc65d0292d8cd30f837c6f450257e
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2021-09-07 11:18:18 +0700

    add config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Career HMIF ITB`,
+    description: `Career. Commitment. Growth`,
+    siteUrl: `https://careers.hmif.tech/`,
   },
   plugins: [
     `gatsby-plugin-image`,
